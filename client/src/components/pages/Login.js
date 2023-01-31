226050984f4547fc3219510f7d0de4b609974ba4
Obi-Nnamdi <59924301+Obi-Nnamdi@users.noreply.github.com>
2023-01-31 18:37:29 -0500

    Updated Login Page

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -2,6 +2,9 @@ import React from "react";
 import NavBar from "./NavBar";
 import Feed from "./Feed";
 
+import InstaspamLogo from "../../public/icons/Instaspam Logo.svg";
+import MITLogo from "../../public/icons/MIT-logo-white-white-72x38.svg";
+
 import "../../utilities.css";
 import { get, post } from "../../utilities";
 import "./Login.css";
@@ -21,25 +24,26 @@ const login = () => {
     window.location.href = response.authCodeUrl;
   });
 };
-
-const logout = () => {
-  console.log("Logging Out");
-  post("/auth/signout").then(window.location.reload());
-};
-
 /**
  * Login is the component responsible for displaying the login page for the user
  */
 const Login = (props) => {
   return (
     <>
-      <p>Login Page!</p>
-      <button onClick={login}>MSAL Login</button>
-      {/* {props.userID ? (
-        <button onClick={logout}>MSAL Logout</button>
-      ) : (
-        <button onClick={login}>MSAL Login</button>
-      )} */}
+      <div className="u-flex u-flex-justifyCenter u-flex-alignCenter Login-header">
+        <img src={InstaspamLogo} alt="Instaspam Logo" />
+        <h1>Instaspam</h1>
+      </div>
+      <div className="u-flexColumn u-flex-alignCenter">
+        <h2>Log In to Your Account</h2>
+        <button onClick={login} className="Login-buttonPrompt u-pointer">
+          {/* This extra span prevents the jittering effect that can happen on hover*/}
+          <span className="u-flex u-flex-justifyCenter u-flex-alignCenter Login-buttonContent">
+            <img src={MITLogo} alt="MIT Logo" />
+            <p>Continue with MIT Outlook</p>
+          </span>
+        </button>
+      </div>
     </>
   );
 };
