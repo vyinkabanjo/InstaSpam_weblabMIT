72332b20d1acdee936492dd9e2c4dd3631a4d614
vyinkabanjo <vyinkabanjo@gmail.com>
2023-01-30 23:43:28 -0500

    only get emails from inbox

--- server/authConfig.js
+++ server/authConfig.js
@@ -33,7 +33,7 @@ const msalConfig = {
 
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI;
-const GRAPH_ME_ENDPOINT = process.env.GRAPH_API_ENDPOINT + "v1.0/me";
+const GRAPH_ME_ENDPOINT = process.env.GRAPH_API_ENDPOINT + "v1.0/me/mailFolders/inbox";
 
 module.exports = {
   msalConfig,
