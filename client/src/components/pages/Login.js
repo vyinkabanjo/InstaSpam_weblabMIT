649e4fd178114675c63fab911a370aabe0f07479
Obi-Nnamdi <59924301+Obi-Nnamdi@users.noreply.github.com>
2023-01-31 21:06:53 -0500

    Updated Login Page and made it more reactive

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -4,6 +4,7 @@ import Feed from "./Feed";
 
 import InstaspamLogo from "../../public/icons/Instaspam Logo.svg";
 import MITLogo from "../../public/icons/MIT-logo-white-white-72x38.svg";
+import StarVector from "../../public/icons/Star Vector.svg";
 
 import "../../utilities.css";
 import { get, post } from "../../utilities";
@@ -30,19 +31,25 @@ const login = () => {
 const Login = (props) => {
   return (
     <>
-      <div className="u-flex u-flex-justifyCenter u-flex-alignCenter Login-header">
-        <img src={InstaspamLogo} alt="Instaspam Logo" />
-        <h1>Instaspam</h1>
-      </div>
-      <div className="u-flexColumn u-flex-alignCenter">
-        <h2>Log In to Your Account</h2>
-        <button onClick={login} className="Login-buttonPrompt u-pointer">
-          {/* This extra span prevents the jittering effect that can happen on hover*/}
-          <span className="u-flex u-flex-justifyCenter u-flex-alignCenter Login-buttonContent">
-            <img src={MITLogo} alt="MIT Logo" />
-            <p>Continue with MIT Outlook</p>
-          </span>
-        </button>
+      <div className="u-hideOverflow u-maxHeight Login-container">
+        <div className="u-flexColumn Login-textContainer">
+          <div className="u-flex u-flex-justifyCenter u-flex-alignCenter Login-header">
+            <img src={InstaspamLogo} alt="Instaspam Logo" />
+            <h1>Instaspam</h1>
+          </div>
+          <div className="u-flexColumn u-flex-alignCenter u-flex-justifyCenter">
+            <h2 className="Login-prompt">Log In to Your Account</h2>
+            <button onClick={login} className="Login-buttonPrompt u-pointer">
+              {/* This extra span prevents the jittering effect that can happen on hover*/}
+              <span className="u-flex u-flex-justifyCenter u-flex-alignCenter Login-buttonContent">
+                <img src={MITLogo} alt="MIT Logo" />
+                <p>Continue with MIT Outlook</p>
+              </span>
+            </button>
+          </div>
+        </div>
+        <img src={StarVector} id="Login-star-bottomLeft"></img>
+        <img src={StarVector} id="Login-star-topRight"></img>
       </div>
     </>
   );
